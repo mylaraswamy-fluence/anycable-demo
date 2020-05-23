19382c1659ce231e299b8595300b2770aac99279
Mylara <mylaraswamy.kuruba@effone.com>
2020-05-23 13:08:02 +0530

    Added chnages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ Rails.application.configure do
   # Set Action Cable server url for consumer connection
   # config.action_cable.url = 'ws://cable.example.com:28080'
   config.action_cable.url = 'ws://anycable-demo-cable.herokuapp.com:3334/cable'
+  
 end
